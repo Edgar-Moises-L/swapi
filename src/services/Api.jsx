a670915e667a4a9ac1c55586ba571039b0bdf31e
Edgar-Moises-L <moisesleyva202@gmail.com>
2025-08-15 17:14:17 -0700

    refactorizado del consumo de los datos fetchPlaneta

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,4 +1,5 @@
 const BASE_URL = 'https://swapi.info/api/people';
+const cachePlanetas = {};
 
 export const fetchPersonajes = async () => {
     try {
@@ -11,18 +12,43 @@ export const fetchPersonajes = async () => {
     }
 };
 
-export const PersonajesMapper = async () => {
-    const data = await fetchPersonajes();
-    return data.map(p => ({
-        nombre: p.name,
-        altura: p.height,
-        peso: p.mass,
-        colorCabello: p.hair_color,
-        colorPiel: p.skin_color,
-        colorOjos: p.eye_color,
-        fechaNacimiento: p.birth_year,
-        genero: p.gender,
-        planetaNacimiento: p.homeworld
-    }));
+export const fetchPlaneta = async (url) => {
+    if (cachePlanetas[url]) {
+        return cachePlanetas[url];
+    }
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        cachePlanetas[url] = data.name;
+        return data.name; 
+    } catch (error) {
+        console.error(error);
+        return "Desconocido";
+    }
 };
 
+
+export const PersonajesMapper = async () => {
+    const data = await fetchPersonajes();
+    const personajesMappeados = await Promise.all(
+
+        data.map(async p => {
+            const nombrePlaneta = await fetchPlaneta(p.homeworld);
+
+            return {
+                nombre: p.name,
+                altura: p.height,
+                peso: p.mass,
+                colorCabello: p.hair_color,
+                colorPiel: p.skin_color,
+                colorOjos: p.eye_color,
+                fechaNacimiento: p.birth_year,
+                genero: p.gender,
+                planetaNacimiento: nombrePlaneta
+            };
+        })
+    );
+
+    return personajesMappeados;
+};
